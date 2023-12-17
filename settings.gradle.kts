dfaaad78b2dcbe0c1e8104c282c5f6a3284228e8
Marpio <marpiono@gmail.com>
2023-12-17 15:57:42 +0700

    UTS

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Calculator(Marpio N.O_C2157201035)"
+include(":app")
+ 
